8966b5b3c3830579e2e2f77369336b8ac3ce547d
AsilbekYTGemes <murotasilbek7@gmail.com>
2025-12-23 23:06:22 +0500

    Versiya 3.0: o'zgartirishlar tavsifi

--- shop/main/admin.py
+++ shop/main/admin.py
@@ -11,3 +11,4 @@ class ProductAdmin (admin.ModelAdmin):
     list_display =  ['name', 'category', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated', 'category']
     list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
