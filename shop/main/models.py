283ce3435db9c4aa4a503073f0e65e4aa963681c
AsilbekYTGemes <murotasilbek7@gmail.com>
2025-12-23 22:35:48 +0500

    Versiya 3.0: o'zgartirishlar tavsifi

--- shop/main/models.py
+++ shop/main/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100,db_index=True)
+    slug = models.SlugField(max_length=100,unique=True)
